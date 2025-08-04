8e47cbba001265c12c6d38297552500f5224b3b7
hefnereb <143023008+hefnereb@users.noreply.github.com>
2025-08-04 11:03:13 -0400

    final update

--- magic_number_examples/src/main/java/edu/appstate/cs/examples/BadCode.java
+++ magic_number_examples/src/main/java/edu/appstate/cs/examples/BadCode.java
@@ -1,16 +1,16 @@
-package edu.appstate.cs.examples;
+// package edu.appstate.cs.examples;
 
-public class BadCode {
-    public static void someMethod() {
-        int y = 1;
-        if (y == 25) {
-            System.out.println("I'm here");
-        }
+// public class BadCode {
+//     public static void someMethod() {
+//         int y = 1;
+//         if (y == 25) {
+//             System.out.println("I'm here");
+//         }
 
-        if (Math.random() < 0.5) {
-            System.out.println("I'm here 2");
-        } else {
-            System.out.println("I'm here 3");
-        }
-    }
-}
+//         if (Math.random() < 0.5) {
+//             System.out.println("I'm here 2");
+//         } else {
+//             System.out.println("I'm here 3");
+//         }
+//     }
+// }
--- magic_number_examples/src/main/java/edu/appstate/cs/examples/MagicNumber.java
+++ magic_number_examples/src/main/java/edu/appstate/cs/examples/MagicNumber.java
@@ -1,45 +1,45 @@
-/*
- * A sample file for the checker to process.
- */
+// /*
+//  * A sample file for the checker to process.
+//  */
 
- package edu.appstate.cs.examples;
+//  package edu.appstate.cs.examples;
 
- public class MagicNumber {
-   public void foo(String s) {
-     System.out.println(s);
-   }
+//  public class MagicNumber {
+//    public void foo(String s) {
+//      System.out.println(s);
+//    }
  
-   public void thisMethodNameIsRidiculouslyLongAndShouldAlsoBeFlaggedByOurPluginBecauseItKeepsGoing() {
-     MagicNumber thisOtherNameIsWayTooLongAndShouldBeFlaggedByTHEPluginBecauseItIsOverFifty = new MagicNumber();
-     thisOtherNameIsWayTooLongAndShouldBeFlaggedByTHEPluginBecauseItIsOverFifty.foo("bad name");
-     MagicNumber aaaaaaa = new MagicNumber();
-     aaaaaaa.foo("bad name");
-   }
+//    public void thisMethodNameIsRidiculouslyLongAndShouldAlsoBeFlaggedByOurPluginBecauseItKeepsGoing() {
+//      MagicNumber thisOtherNameIsWayTooLongAndShouldBeFlaggedByTHEPluginBecauseItIsOverFifty = new MagicNumber();
+//      thisOtherNameIsWayTooLongAndShouldBeFlaggedByTHEPluginBecauseItIsOverFifty.foo("bad name");
+//      MagicNumber aaaaaaa = new MagicNumber();
+//      aaaaaaa.foo("bad name");
+//    }
  
-   public static void main(String[] args) {
-     String m = "This is a message";
-     int d = 5;
-     int dd = 1; 
-     if (dd > 2)
-    {
+//    public static void main(String[] args) {
+//      String m = "This is a message";
+//      int d = 5;
+//      int dd = 1; 
+//      if (dd > 2)
+//     {
       
-    }
-     if (d < 3)
-     {
-        System.out.println("d is greater than 3");
-     }
-     else if (dd < 2)
-     {
-        System.out.println("dd is less than 2");
-     }
-     else
-     {
-        System.out.println("d is not greater than 3 and dd is not less than 2");
-     }
+//     }
+//      if (d < 3)
+//      {
+//         System.out.println("d is greater than 3");
+//      }
+//      else if (dd < 2)
+//      {
+//         System.out.println("dd is less than 2");
+//      }
+//      else
+//      {
+//         System.out.println("d is not greater than 3 and dd is not less than 2");
+//      }
 
-     MagicNumber b = new MagicNumber();
-     MagicNumber ccc = new MagicNumber();
-     ccc.foo(m);
-     b.foo(m);
-   }
- }
+//      MagicNumber b = new MagicNumber();
+//      MagicNumber ccc = new MagicNumber();
+//      ccc.foo(m);
+//      b.foo(m);
+//    }
+//  }
